0dca3984d46ab3750e13c54e09bb26f96fda1dfa
Kuno Tatemaki <raulfeliz@gmail.com>
2017-08-24 19:27:04 +0200

    create custom scope

---
+++ mobile/src/main/java/com/rukiasoft/amaristest/dependencyInjection/scopes/CustomScopes.kt
@@ -0,0 +1,12 @@
+package com.rukiasoft.amaristest.dependencyInjection.scopes
+
+import javax.inject.Scope
+
+/**
+ * Created by Roll on 24/8/17.
+ */
+interface CustomScopes {
+
+    @Scope
+    annotation class ActivityScope
+}
